33770ad9848a25c7b8fff008a99391e5526a1e45
Jeffrey Cheung <jeffreyy.cheungg@gmail.com>
2018-06-12 16:28:22 -0400

    sort by strings

--- sortByStrings.js
+++ sortByStrings.js
@@ -1,11 +1,27 @@
 function sortByStrings(s,t) {
   let sLetters = {};
+  let tLetters = {};
   let returnString = '';
+  let remaining = '';
+  for (let i = 0; i < t.length; i++) {
+    if (tLetters[[i]]) {
+      tLetters[t[i]]++;
+    } else {
+      tLetters[t[i]] = 1;
+    }
+  }
   for (let i = 0; i < s.length; i++) {
     if (sLetters[s[i]]) {
       sLetters[s[i]]++;
     } else {
       sLetters[s[i]] = 1;
+    }
+  }
+  for (let letter in sLetters) {
+    if (!tLetters[letter]) {
+      for (let i = 0; i < sLetters[letter]; i++) {
+        remaining += letter;
+      }
     }
   }
   for (let i = 0; i < t.length; i++) {
@@ -15,5 +31,6 @@ function sortByStrings(s,t) {
       }
     }
   }
+  returnString += remaining;
   return returnString;
 }
